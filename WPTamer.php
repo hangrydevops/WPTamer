bb69d82d0b6b93abd3717f548c3371a5e6f5f96a
Devin <67713580+hangrydevops@users.noreply.github.com>
2020-07-01 23:42:24 -0500

    Create WPTamer.php

---
+++ WPTamer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: WPTamer
+ * Plugin URI: http://www.ootwsolutions.com/WPTamer
+ * Description: This plugin unfolds the mysteries of your MIND
+ * Version: 1.0
+ * Author: HangryDevOps
+ * Author URI: http://www.ootwsoltutions.com
+ */
+ 
+ add_action( 'the_content', 'rage' );
+
+function rage ( $content ) {
+    return $content .= '<p>You have angered Cthulu</p>';
+}
